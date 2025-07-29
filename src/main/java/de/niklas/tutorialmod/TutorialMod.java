a7b8643a329ebe5c43945792dc379c736d7d2291
xQuicklook <Niklas_Noch@t-online.de>
2025-07-29 21:03:27 +0200

    Blocks added

--- src/main/java/de/niklas/tutorialmod/TutorialMod.java
+++ src/main/java/de/niklas/tutorialmod/TutorialMod.java
@@ -1,6 +1,10 @@
 package de.niklas.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import de.niklas.tutorialmod.block.ModBlocks;
+import de.niklas.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -35,6 +39,9 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -50,7 +57,14 @@ public class TutorialMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+    if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+        event.accept(ModItems.ALEXANDRITE);
+        event.accept(ModItems.RAW_ALEXANDRITE);
+    }
+    if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+        event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+        event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
+    }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
