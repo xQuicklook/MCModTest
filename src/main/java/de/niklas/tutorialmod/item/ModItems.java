c8e3c40b3e97ff0d1d2b0c7831d14f09940e83dd
xQuicklook <Niklas_Noch@t-online.de>
2025-08-02 14:07:07 +0200

    Custom Item

--- src/main/java/de/niklas/tutorialmod/item/ModItems.java
+++ src/main/java/de/niklas/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package de.niklas.tutorialmod.item;
 
 import de.niklas.tutorialmod.TutorialMod;
+import de.niklas.tutorialmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,8 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
